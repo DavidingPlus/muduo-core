8218ee02b417973ea70ab52bf1fa145cd98db2e3
DavidingPlus <davidingplus@qq.com>
2026-07-20 20:58:04 +0800

    add test for disabled

--- test/testrun/testrun.cpp
+++ test/testrun/testrun.cpp
@@ -5,3 +5,9 @@ TEST(TestRunTests, TestRun)
 {
     SUCCEED();
 }
+
+// 以 DISABLED_ 开头的测试会被 GTest 统计到 disabled，但默认不会执行。这里故意写成 FAIL()，用来证明它不会影响最终通过结果。
+TEST(TestRunTests, DISABLED_ExampleDisabledTest)
+{
+    FAIL();
+}
