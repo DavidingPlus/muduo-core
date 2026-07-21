198c55dd63ebfa3f693055b781d9f1811a4651d8
DavidingPlus <davidingplus@qq.com>
2026-07-21 16:35:35 +0800

    working in poll

--- src/core/epollpoller.cpp
+++ src/core/epollpoller.cpp
@@ -1,5 +1,6 @@
 #include "epollpoller.h"
 
+#include "channel.h"
 #include "logger.h"
 #include "timestamp.h"
 
@@ -10,7 +11,6 @@ EPollPoller::EPollPoller(EventLoop *loop)
     if (m_epollfd < 0) LOG_FATAL("epoll_create error: {}", errno);
 }
 
-// TODO
 Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels)
 {
     /**
@@ -19,6 +19,42 @@ Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels)
      * 2. epoll_ctl (add, mod, del)
      * 3. epoll_wait
      **/
+
+    // 由于频繁调用 poll，用 LOG_DEBUG 输出日志更好一点更为合理。当遇到并发场景，关闭 DEBUG 日志提升效率。
+    LOG_DEBUG("func={} -> fd total count: {}", __FUNCTION__, m_channels.size());
+
+    int saveErrno = errno;
+    Timestamp now(Timestamp::now());
+
+    // epoll_wait 会阻塞等待已注册的文件描述符事件发生。m_events 是用户空间保存 epoll 返回事件的缓冲区。每次调用 epoll_wait 时，内核都会重新向该数组写入当前发生的事件，并不会追加上一次的数据。返回值 numEvents 表示本次有效事件数量，因此只需要处理 m_events[0, numEvents - 1]。如果 numEvents 等于 m_events 当前容量，说明用户提供的事件数组可能被填满，epoll 中可能还有更多就绪事件没有返回，内核会处理让这部分事件会在下次 epoll_wait 的时候返回。因此扩大 m_events 容量，让下一次 epoll_wait 可以接收更多事件，尝试提高效率。
+    int numEvents = ::epoll_wait(m_epollfd, m_events.data(), static_cast<int>(m_events.size()), timeoutMs);
+    if (numEvents > 0)
+    {
+        LOG_DEBUG("%d events happend\n", numEvents);
+
+        // 把监听到的 Channel 装进 activeChannels 中（它是一个 vector<Channel*>）。这样，当外界调用完 poll 之后就能拿到事件监听器的监听结果。（activeChannels）。
+        fillActiveChannels(numEvents, activeChannels);
+
+        // 扩容操作。
+        if (m_events.size() == numEvents) m_events.resize(m_events.size() * 2);
+    }
+    // 返回值为 0，代表超时。
+    else if (0 == numEvents)
+    {
+        LOG_DEBUG("%s timeout!", __FUNCTION__);
+    }
+    else
+    {
+        // epoll_wait 被信号打断（EINTR），不算真正错误，下次重试即可。其他情况就是错误。
+        if (EINTR != saveErrno)
+        {
+            errno = saveErrno;
+            LOG_ERROR("EPollPoller::poll() error!");
+        }
+    }
+
+
+    return now;
 }
 
 void EPollPoller::updateChannel(Channel *channel)
@@ -31,6 +67,14 @@ void EPollPoller::removeChannel(Channel *channel)
 
 void EPollPoller::fillActiveChannels(int numEvents, ChannelList *activeChannels) const
 {
+    for (int i = 0; i < numEvents; ++i)
+    {
+        Channel *channel = reinterpret_cast<Channel *>(m_events[i].data.ptr);
+        channel->setRevents(m_events[i].events);
+        activeChannels->emplace_back(channel);
+
+        // 这样 EventLoop 就拿到了它的 Poller 给它返回的所有发生事件的 channel 列表了。
+    }
 }
 
 void EPollPoller::update(int operation, Channel *channel)
--- src/core/epollpoller.h
+++ src/core/epollpoller.h
@@ -23,6 +23,7 @@ public:
 
     // 重写基类 Poller 的抽象方法。
 
+    // 当外部调用 poll 方法的时候，该方法底层其实是通过 epoll_wait 获取这个事件监听器上发生事件的 fd 及其对应发生的事件。我们知道每个 fd 都是由一个 Channel 封装的，通过哈希表 m_channels 可以根据 fd 找到封装这个 fd 的 Channel。将事件监听器监听到该 fd 发生的事件写进这个 Channel 中的 revents 成员变量中。然后把这个 Channel 装进 activeChannels 中（它是一个 vector<Channel*>）。这样，当外界调用完 poll 之后就能拿到事件监听器的监听结果（activeChannels）。这个  activeChannels 就是事件监听器监听到的发生事件的 fd，以及每个 fd 都发生了什么事件。
     Timestamp poll(int timeoutMs, ChannelList *activeChannels) override;
 
     void updateChannel(Channel *channel) override;
@@ -38,7 +39,7 @@ private:
     static constexpr int kInitEventListSize = 16;
 
 
-    // 填写活跃的连接。
+    // 将 epoll 返回的就绪事件转换成 Channel 列表。epoll_wait 返回的是 epoll_event 数组，其中保存了发生事件的文件描述符信息。在注册 epoll 事件时，通过 event.data.ptr 保存了对应的 Channel 指针，所以这里可以直接取出 Channel 对象，无需再通过 fd 查找。同时将实际发生的事件保存到 Channel 的 m_revents 成员中，供 EventLoop 后续处理。Channel 列表通过入参指针 activeChannels 传出。
     void fillActiveChannels(int numEvents, ChannelList *activeChannels) const;
 
     // 更新 channel 通道，其实就是调用 epoll_ctl。
