385c42ee662a99e2217510b1862b22fa26de4f3f
DavidingPlus <davidingplus@qq.com>
2026-07-21 19:38:42 +0800

    working on ctor and dtor of EventLoop

--- src/core/eventloop.cpp
+++ src/core/eventloop.cpp
@@ -1,14 +1,58 @@
 #include "eventloop.h"
 
 #include "poller.h"
+#include "channel.h"
+#include "logger.h"
 
+#include <cassert>
+
+#include <sys/eventfd.h>
+
+
+// 线程局部变量，一个线程只有一个。防止一个线程创建多个 EventLoop。
+static thread_local EventLoop *t_loopInThisThread = nullptr;
+
+
+int EventLoop::createEventfd()
+{
+    int evtfd = ::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
+    if (evtfd < 0) LOG_FATAL("eventfd error: {}", errno);
+    return evtfd;
+}
 
 EventLoop::EventLoop()
+    : m_poller(Poller::newDefaultPoller(this)), m_wakeupFd(createEventfd()), m_wakeupChannel(new Channel(this, m_wakeupFd))
 {
+    if (t_loopInThisThread)
+    {
+        LOG_FATAL("Another EventLoop {} exists in this thread {}", fmt::ptr(t_loopInThisThread), m_threadId);
+    }
+    else
+    {
+        t_loopInThisThread = this;
+    }
+
+    LOG_DEBUG("EventLoop created {} in thread {}", fmt::ptr(this), m_threadId);
+
+    // 和 pipe() 管道类似，eventfd 用于 EventLoop 线程间通信。而 m_wakeupFd 就是专门用于存储 eventfd 的文件描述符。
+    // 当一个线程需要唤醒另一个 EventLoop 时，通过 write(eventfd) 写入通知。目标 EventLoop 将 eventfd 封装为 Channel，并监听 EPOLLIN 事件。eventfd 被写入后变为可读状态，epoll_wait 返回，EventLoop 被唤醒，随后通过 handleRead() 清空 eventfd，并执行 pendingFunctors 中的任务。每一个 EventLoop 都将监听 m_wakeupChannel 的 EPOLL 读事件了。
+    m_wakeupChannel->enableReading();
+    // 设置 wakeupFd 的事件类型以及发生事件后的回调操作。
+    m_wakeupChannel->setReadCallback(std::bind(&EventLoop::handleRead, this));
 }
 
 EventLoop::~EventLoop()
 {
+    assert(!m_looping);
+
+    // 给 Channel 移除所有感兴趣的事件。
+    m_wakeupChannel->disableAll();
+    // 把 Channel 从 EventLoop 上删除掉。
+    m_wakeupChannel->remove();
+
+    ::close(m_wakeupFd);
+
+    t_loopInThisThread = nullptr;
 }
 
 void EventLoop::loop()
@@ -29,6 +73,9 @@ void EventLoop::queueInLoop(EventLoop::Functor cb)
 
 void EventLoop::wakeup()
 {
+    uint64_t one = 1;
+    ssize_t n = ::write(m_wakeupFd, &one, sizeof(one));
+    if (sizeof(one) != n) LOG_ERROR("EventLoop::wakeup() writes {} bytes instead of 8", n);
 }
 
 void EventLoop::updateChannel(Channel *channel)
@@ -48,6 +95,9 @@ bool EventLoop::hasChannel(Channel *channel)
 
 void EventLoop::handleRead()
 {
+    uint64_t one = 1;
+    ssize_t n = ::read(m_wakeupFd, &one, sizeof(one));
+    if (sizeof(one) != n) LOG_ERROR("EventLoop::handleRead() reads {} bytes instead of 8", n);
 }
 
 void EventLoop::doPendingFunctors()
--- src/core/eventloop.h
+++ src/core/eventloop.h
@@ -45,7 +45,7 @@ public:
     // 把上层注册的回调函数 cb 放入队列中，唤醒 loop 所在的线程执行 cb。
     void queueInLoop(Functor cb);
 
-    // 通过 eventfd 唤醒 loop 所在的线程。
+    // 通过 eventfd 唤醒 loop 所在的线程。向 m_wakeupFd 写一个数据，wakeupChannel 就发生读事件，当前 loop 线程就会被唤醒。
     void wakeup();
 
     // EventLoop 的方法，内部是在调用 Poller 的方法。
@@ -61,6 +61,23 @@ public:
 
 private:
 
+    /* 创建线程之后主线程和子线程谁先运行是不确定的。通过一个 eventfd 在线程之间传递数据的好处是多个线程无需上锁就可以实现同步。eventfd 支持的最低内核版本为 Linux 2.6.27，在 2.6.26 及之前的版本也可以使用 eventfd，但是 flags 必须设置为 0。
+     * 函数原型：
+     *     #include <sys/eventfd.h>
+     *     int eventfd(unsigned int initval, int flags);
+     * 参数说明：
+     *      initval：初始化计数器的值。
+     *      flags：EFD_NONBLOCK,设置 socket 为非阻塞。EFD_CLOEXEC，执行 fork 的时候，在父进程中的描述符会自动关闭，子进程中的描述符保留。
+     * 场景：
+     *     eventfd 可以用于同一个进程之中的线程之间的通信。
+     *     eventfd 还可以用于同亲缘关系的进程之间的通信。
+     *     eventfd 用于不同亲缘关系的进程之间通信的话需要把 eventfd 放在几个进程共享的共享内存中（没有测试过）。
+     */
+
+    // 创建 wakeupfd 用来 notify 唤醒 subReactor 处理新来的 channel。
+    static int createEventfd();
+
+
     // 给 eventfd 返回的文件描述符 m_wakeupFd 绑定的事件回调。当 wakeup() 时，即有事件发生时调用 handleRead() 读 m_wakeupFd 的 8 字节，同时唤醒阻塞的 epoll_wait。
     void handleRead();
 
@@ -71,10 +88,12 @@ private:
     using ChannelList = std::vector<Channel *>;
 
     // 原子操作，底层通过 CAS 实现。
-    std::atomic_bool m_looping;
+
+    // 标识事件循环是否正在运行。
+    std::atomic_bool m_looping = false;
 
     // 标识退出 loop 循环。
-    std::atomic_bool m_quit;
+    std::atomic_bool m_quit = false;
 
     // 记录当前 EventLoop 是被哪个线程 id 创建的，即标识了当前 EventLoop 的所属线程 id。
     const pid_t m_threadId = CurrentThread::tid();
@@ -84,8 +103,9 @@ private:
 
     std::unique_ptr<Poller> m_poller;
 
-    // 作用：当 mainLoop 获取一个新用户的 Channel 需通过轮询算法选择一个 subLoop 通过该成员唤醒 subLoop 处理 Channel。
-    int m_wakeupFd;
+    // 当 mainLoop 需要处理一个新 Channel 时，需通过轮询算法选择一个 subLoop，同时唤醒 subLoop 处理 Channel。
+    // m_wakeupFd 就是一个专门用来唤醒 epoll_wait() 的文件描述符。
+    int m_wakeupFd = -1;
 
     std::unique_ptr<Channel> m_wakeupChannel;
 
@@ -93,7 +113,7 @@ private:
     ChannelList m_activeChannels;
 
     // 标识当前 loop 是否有需要执行的回调操作。
-    std::atomic_bool m_callingPendingFunctors;
+    std::atomic_bool m_callingPendingFunctors = false;
 
     // 存储 loop 需要执行的所有回调操作。
     std::vector<Functor> m_pendingFunctors;
